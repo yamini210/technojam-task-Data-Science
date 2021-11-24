3b2c5f5cdf70dbf58722f17972fd1551a0cff03f
yamini210 <94973017+yamini210@users.noreply.github.com>
2021-11-24 21:06:13 +0530

    Add files via upload

---
+++ technojam task1.py
@@ -0,0 +1,15 @@
+my_list=[0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1]
+sample_url='http://google.com'
+print(sample_url)
+#reverse
+a=sample_url[::-1]
+print(a)
+print(sample_url[::-1])
+print(sample_url[-4:])
+print(sample_url[7:17])
+print(sample_url[7:])
+print(sample_url[7:-4])
+print(my_list[::-1])
+
+      
+      
